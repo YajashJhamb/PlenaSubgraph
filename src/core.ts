fefd19ee5d5a974c882f082a7dde46ec4612ba58
Yajash Jhamb <yajashjhamb@Yajashs-MacBook-Pro.local>
2022-06-09 19:27:46 +0530

    fixes graphql

--- src/core.ts
+++ src/core.ts
@@ -20,7 +20,7 @@ import {
   Transfer,
   Transaction,
   Withdraw,
-  WithdrawAnyTokens,
+  WithdrawAnyToken,
   Supply,
   UnStake as UnStakeSchema,
   Stake as StakeSchema,
@@ -601,14 +601,14 @@ export function handleZapIn(event: ZapIn): void {
 export function handleClaimedAnyTokens(event: ClaimedAnyTokens): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = WithdrawAnyTokens.load(
+  let entity = WithdrawAnyToken.load(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new WithdrawAnyTokens(
+    entity = new WithdrawAnyToken(
       event.transaction.hash.toHex() + "-" + event.logIndex.toString()
     );
   }
